ce3da65594b0c3a007486482e5a67bcb6cfc46bf
Costbound <popovskiy96@gmail.com>
2023-11-11 07:24:32 -0300

    metric calculator done

--- script.js
+++ script.js
@@ -19,10 +19,10 @@ const idealPara = document.querySelector(".result__ideal-weight-para");
 const idealSpan = document.querySelector(".result__ideal-weight-span");
 
 
-radioContainer.addEventListener("change", async () => {
+radioContainer.addEventListener("change",async () => {
     if (radioMetric.checked) {
-        form.style.maxHeight = "448px";
-        await new Promise(resolve => setTimeout(resolve, 150));
+        form.style.maxHeight = "450px";
+        await delay(500);
         imperialContainer.classList.add("not-active");
         metricContainer.classList.remove("not-active");
         ftInput.value = "";
@@ -41,19 +41,63 @@ radioContainer.addEventListener("change", async () => {
 metricContainer.addEventListener("input", metricCalculate);
 imperialContainer.addEventListener("input", imperialCalculate);
 
+const delay = async (ms) => { await new Promise(resolve => setTimeout(resolve, ms)) }
+
 function inputValidator(evt) {
     return !(evt.keyCode < 48 || evt.keyCode > 57) || (evt.keyCode === 46 && !evt.target.value.includes("."));
 }
 
-function metricCalculate() {
-    if (cmInput.value.length > 0 && kgInput.value.length > 0) {
-        resultContainer.style.flexDirection = "row";
-        resultContainer.style.maxHeight = "166px";
+async function resultDefault() {
+    resultContainer.style.maxHeight = "";
+    await delay(500);
+    resultContainer.style.flexDirection = "";
+    resultContainer.style.alighnItems = "";
+    resultContainer.style.alignItems = "";
+    resultPara.textContent = "Welcome!";
+    resultPara.style.fontSize = "";
+    idealPara.textContent = "Enter your height and weight and you’ll see your BMI result here";
+    idealPara.style.width = "";
+}
+async function resultInvalid() {
+    resultContainer.style.maxHeight = "";
+    await delay(500);
+    resultContainer.style.flexDirection = "";
+    resultContainer.style.alighnItems = "";
+    resultContainer.style.alignItems = "";
+    resultPara.textContent = "Please!";
+    resultPara.style.fontSize = "";
+    idealPara.textContent = "Enter proper height and weight";
+    idealPara.style.width = "";
+}
+async function metricCalculate() {
+    const bmi = Number(kgInput.value / ((cmInput.value / 100) ** 2)).toFixed(1);
+    console.log(typeof(Number(bmi)));
+    if (cmInput.value.length > 1 && kgInput.value.length > 1) {
+        form.style.maxHeight = "480px";
 
-        resultPara.textContent = "Your BMI is...";
-        resultPara.style.fontSize = ""
+        resultContainer.style.flexDirection = "row";
+        resultContainer.style.alighnItems = "center";
+        resultContainer.style.maxHeight = "166px";
+        resultContainer.style.alignItems = "center";
+
+        resultPara.innerHTML = `Your BMI is...<span class="result__span">${bmi}<span>`;
+        resultPara.style.fontSize = "16px";
+
+        idealPara.style.width = "206px";
+        
+        const minIdealWeight = (24.9 * ((Number(cmInput.value) / 100) ** 2)).toFixed(1);
+        const maxIdealWeight = (18.5 * ((Number(cmInput.value) / 100) ** 2)).toFixed(1);
+
+        const fat = bmi < 18.5 ? "underweight" : bmi <= 24.9 ? "healthy weight" : bmi <= 29.9 ? "overweight" : bmi <= 34.9 ? "obese" : bmi <= 39.9 ? "severely obese" : "morbidly obese";
+        idealPara.innerHTML = `Your BMI suggests you’re a ${fat}. Your ideal weight is between <span class="result__ideal-weight-span">${maxIdealWeight}kgs - ${minIdealWeight}kgs</span>.`;
+    } else if (cmInput.value.length === 0 || kgInput.value.length === 0) {
+        resultDefault();
+    }if ((Number(bmi) < 10 || Number(bmi) > 100) && cmInput.value.length > 1 && kgInput.value.length > 1) {
+        resultInvalid();
     }
 }
-function imperialCalculate() {
 
+function imperialCalculate() {
+    const bmi = Number(703 * ((lbsInput.value + stInput.value * 14) / (ftInput.value + (inchInput.value * 0,833333 / 10) ** 2)));
+    console.log(bmi);
 }
